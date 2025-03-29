27db0201fb9bf06493174852268569cf36b6c4ac
unknown <18756859+brandonholderman@users.noreply.github.com>
2025-03-29 01:51:56 -0700

    added models for users, posts and notifications; as well as templates for core and posts

--- core/views.py
+++ core/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import TemplateView, ListView
+
+class HomeView(TemplateView):
+    """Home view class."""
+    
+    template_name = 'index.html'
