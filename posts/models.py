1b0c02cd00a9936eeb91a8699f4f631361d7ed5e
unknown <18756859+brandonholderman@users.noreply.github.com>
2025-03-28 22:37:44 -0700

    added basic file structure

---
+++ posts/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.conf import settings
+
+class Post(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content = models.TextField()
+    image = models.ImageField(upload_to='posts/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Like(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes')
+    created_at = models.DateTimeField(auto_now_add=True)
